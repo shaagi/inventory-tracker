0b344f6a9a70b86a3b217437e8d0b780cf7fbb82
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-17 10:28:15 -0400

    altered styling to make it a bit more bearable when looking at it

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -28,14 +28,14 @@ const Home = () => {
 
   return (
     <div>
-        <h1>Clothing shop Inventory tracker</h1>
-      <div>
+        <h1 className="flex-auto text-lg font-semibold text-center">Clothing shop Inventory tracker</h1>
+      <div className="text-center  space-y-4">
         {inventoryList.map((clothingItem) => (
-          <div className="book" key={clothingItem.productId}>
+          <div className=" pt-6 md:p-8 md:text-left" key={clothingItem.productId}>
            
             <h2>Item Name: {clothingItem.name}</h2>
-            <p>Quantity in stock: {clothingItem.quantity}</p>
-            <span>Item price: ${clothingItem.price}</span>
+            <p>Quantity in stock: {clothingItem.quantity} units</p>
+            <p>Item price: ${clothingItem.price}</p>
             <Link to={`/edit/${clothingItem.productId}`}>
                 <button className="h-10 px-6 font-semibold rounded-md border">
                     Update Item
