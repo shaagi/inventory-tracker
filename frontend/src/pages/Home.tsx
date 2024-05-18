e34d5846a2012754fa9ae64fde347df72744cb40
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-18 10:33:54 -0400

    made list of inventory items look better

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,8 @@ import axios from "axios"
 import { Link } from "react-router-dom";
 import { productModel } from "../interfaces";
 import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
     const [inventoryList, setInventoryList] = useState<productModel[]>([]);
@@ -32,35 +34,40 @@ const Home = () => {
 
   return (
     <div>
-      <h1>
-        Example heading <Badge bg="secondary">New</Badge>
-      </h1>
-        <h1 className="flex-auto text-lg font-semibold text-center">Clothing shop Inventory tracker</h1>
-      <div className="text-center  space-y-4">
+     
+      <h1 className="flex-auto text-lg font-semibold text-center">Clothing shop Inventory tracker</h1>
+      <div>
         {inventoryList.map((clothingItem) => (
-          <div className=" pt-6 md:p-8 md:text-left" key={clothingItem.productId}>
-           
-            <h2>Item Name: {clothingItem.name}</h2>
-            <p>Quantity in stock: {clothingItem.quantity} units</p>
-            <p>Item price: ${clothingItem.price}</p>
-            <Link to={`/edit/${clothingItem.productId}`}>
-                <button className="h-10 px-6 font-semibold rounded-md border">
-                    Update Item
-                </button>
-            </Link>
-            
-            <button 
-              className="h-10 px-6 font-semibold rounded-md border"
-              onClick={() => handleDelete(clothingItem.productId)}
-            >
-              Delete Item
-            </button>
-           
-            
+          <div className="ms-4 p-3" key={clothingItem.productId}>
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>{clothingItem.name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">${clothingItem.price}</Card.Subtitle>
+              <Card.Text>
+                {clothingItem.quantity} units in stock
+              </Card.Text>
+    
+              <Link to={`/edit/${clothingItem.productId}`}>
+                  <Button variant="secondary">
+                      Update Item
+                  </Button>
+              </Link>
+              {' '}
+              <Button 
+                variant="danger"
+                onClick={() => handleDelete(clothingItem.productId)}
+              >
+                Delete Item
+              </Button>
+            </Card.Body>
+            </Card>
           </div>
         ))}
+        
       </div>
+      
       <div>
+      
         <Link to={`/add`}>
            <button className="h-10 px-6 font-semibold rounded-md border">
               Add Item
