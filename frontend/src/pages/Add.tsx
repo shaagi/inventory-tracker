b6bb4bd55b36cee998be5096100d414040a7ab94
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-18 15:57:31 -0400

    made add page look better

--- frontend/src/pages/Add.tsx
+++ frontend/src/pages/Add.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 const Add = () => {
@@ -26,41 +28,40 @@ const Add = () => {
     <div className="p-4">
       
       <h1 className="text-3x1 my-4">Add Item</h1>
-   
-      <div className="flex flex-col border-2 border-sky-400 rounded-x1 w-[600px] p-4 mx-auto">
-        <div className="my-4">
-          <label className="text-x1 mr-4 text-gray-500">Item name</label>
-          <input
-            type="text"
-            placeholder="socks"
-            onChange={(e) => setName(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-x1 mr-4 text-gray-500">Item quantity</label>
-          <input
-            type="number"
-            placeholder="5"
-            onChange={(e) => setQuantity(+e.target.value)}
-            min="0"
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
-        </div>
-        <div className="my-4">
-          <label className="text-x1 mr-4 text-gray-500">Item Price (in dollars)</label>
-          <input
-            type="number"
-            placeholder="15"
-            min="0"
-            onChange={(e) => setPrice(+e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
-        </div>
-        <button className="p-2 bg-sky-300 m-8" onClick={handleAdd}>
-          Add
-        </button>
-      </div>
+
+      <Form className="p-5">
+      <Form.Group className="mb-3" >
+        <Form.Label>Item name</Form.Label>
+        <Form.Control 
+          type="text"
+          placeholder="socks"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" >
+        <Form.Label>Item Quantity</Form.Label>
+        <Form.Control 
+          type="number"
+          placeholder="5"
+          onChange={(e) => setQuantity(+e.target.value)}
+          min="0"
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" >
+        <Form.Label>Item Price</Form.Label>
+        <Form.Control 
+          type="number"
+          placeholder="15"
+          min="1"
+          onChange={(e) => setPrice(+e.target.value)}
+        />
+      </Form.Group>
+     
+      <Button variant="primary" onClick={handleAdd}>
+        Add
+      </Button>
+    </Form>
     </div>
   )
 }
