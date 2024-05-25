eccb1c32439aab4075703e7d180f48166d7f3017
Swagat Soni <ssoni18@schulich.yorku.ca>
2024-05-25 16:56:17 -0400

    log

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
             try {
                 const res = await axios.get(backend_url + "/inventory");
                 // const res = await axios.get(`${process.env.DB_HOST}`);
-                console.log(res.data);
+                console.log("is you: "+ res.data);
                 setInventoryList(res.data);
             } catch (err) {
                 console.log(err);
